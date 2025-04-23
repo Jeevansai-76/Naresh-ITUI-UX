d928309dc2c0ca07cc5d2144a799cbe6c625cd3e
Jeevansai-76 <jeevansai926@gmail.com>
2025-04-24 01:29:04 +0530

    Started CSS Challenges

--- JS/Datatypes/script.js
+++ JS/Datatypes/script.js
@@ -1,22 +1,88 @@
 
 // 1.number
-let x = 45.233,k=25;
-console.log(typeof x);
-let y = k.toString(2); //convert into string (2 is base like 2,8,16 etc 2 means binary)
-console.log(y);
-console.log(typeof y);
-console.log(typeof x);  // x is immutable so primitivie data types are immutable
-let z = x.toFixed(2); //getting till 2 decimal points then roundof()
-console.log(z); 
+// let x = 45.233,k=25;
+// console.log(typeof x);
+// let y = k.toString(2); //convert into string (2 is base like 2,8,16 etc 2 means binary)
+// console.log(y);
+// console.log(typeof y);
+// console.log(typeof x);  // x is immutable so primitivie data types are immutable
+// let z = x.toFixed(2); //getting till 2 decimal points then roundof()
+// console.log(z); 
 
 //math methods
-let a=64,b=27;
-console.log(Math.sqrt(a));
-console.log(Math.cbrt(a));
-console.log(Math.max(1,23,433,23,121,3233));
-console.log(Math.min(1,23,433,23,121,3233));
-console.log(Math.round(5.24));
-console.log(Math.ceil(-2.45));
-console.log(Math.floor(2.45));
-console.log(Math.ceil(Math.random()*6));
-console.log(Math.abs(-2.34));
+// let a=64,b=27;
+// console.log(Math.sqrt(a));
+// console.log(Math.cbrt(a));
+// console.log(Math.max(1,23,433,23,121,3233));
+// console.log(Math.min(1,23,433,23,121,3233));
+// console.log(Math.round(5.24));
+// console.log(Math.ceil(-2.45));
+// console.log(Math.floor(2.45));
+// console.log(Math.ceil(Math.random()*6));
+// console.log(Math.abs(-2.34));
+
+
+// 2.string
+// let s = 'raj kumar'
+// console.log(s)
+// let s = "raj kumar"
+// console.log(s);
+
+// let s = "Hi I'm the person who thinks creativily";
+// let s = 'Hi I\'m the person who thinks creativily';
+// let s = 'Hi, my name is "Jeevan sai. "';
+// let s = "Hi, my name is \"Jeevan sai.\" ";
+// console.log(s);
+
+// let age  = 22;
+// let n = 'Jeevan Sai';
+// console.log(`my name is ${n} and age is ${age}`);
+
+
+//String methods
+
+// let s = 'Javascript';
+
+// console.log(s.length);
+// console.log(s.toUpperCase());
+// console.log(s.toLowerCase());
+
+// console.log(s.charAt(4));
+// console.log(s.indexOf('v'));
+// console.log(s.lastIndexOf('a'));
+
+// console.log(s.concat(' is easy'));
+// console.log(s.includes('va'));
+
+// let str = "I am the Javascript";
+// console.log(str.split(' '));
+
+// let arr = ['Hi','I','am','the','javascript'];
+// console.log(arr.join(' '));
+
+// console.log(s.startsWith('Ja'));
+// console.log(s.endsWith('ip'));
+
+// console.log(s.replace('a','k'));
+// console.log(s.replaceAll('a','k'));
+
+// console.log(s.slice(4));
+// console.log(s.slice(4,7));
+
+// 3.boolean
+// let a = (1>2);
+// let b = (1==1);
+// console.log(a);
+// console.log(b);
+
+// 4.Undefined
+// console.log(a);//Undefined
+// var a = 10;
+// console.log(a);
+// let arr = [1,2,3];
+// console.log(arr[3]);//Undefined
+// arr[3] = 4;
+
+// 5.Null
+// let a = null;
+// console.log(a);
